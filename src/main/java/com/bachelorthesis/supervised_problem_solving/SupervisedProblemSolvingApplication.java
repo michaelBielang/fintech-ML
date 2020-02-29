e19052fd968a0159af1b806ec284f7a5f29764a4
Michael Bielang <michael.bielang@hs-augsburg.de>
2020-02-29 13:35:53 +0100

    init

---
+++ src/main/java/com/bachelorthesis/supervised_problem_solving/SupervisedProblemSolvingApplication.java
@@ -0,0 +1,13 @@
+package com.bachelorthesis.supervised_problem_solving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervisedProblemSolvingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupervisedProblemSolvingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bachelorthesis/supervised_problem_solving/SupervisedProblemSolvingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bachelorthesis.supervised_problem_solving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisedProblemSolvingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
