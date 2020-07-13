f5f5d8996078fdf03835e83cfd3fd075e30e23f4
Michael Bielang <michael.bielang@hs-augsburg.de>
2020-07-13 19:10:00 +0200

    cleanup

--- src/main/java/com/bachelorthesis/supervised_problem_solving/dl4jApp/Dl4jLinearRegressionService.java
+++ src/main/java/com/bachelorthesis/supervised_problem_solving/dl4jApp/Dl4jLinearRegressionService.java
@@ -2,18 +2,15 @@ package com.bachelorthesis.supervised_problem_solving.dl4jApp;
 
 import com.bachelorthesis.supervised_problem_solving.dl4jApp.vo.ResultSet;
 import com.bachelorthesis.supervised_problem_solving.enums.Indicators;
-import com.bachelorthesis.supervised_problem_solving.services.algos.FactorNames;
+import com.bachelorthesis.supervised_problem_solving.services.algos.MService;
 import com.bachelorthesis.supervised_problem_solving.services.exchangeAPI.poloniex.enums.Periods;
 import com.bachelorthesis.supervised_problem_solving.services.exchangeAPI.poloniex.vo.ChartDataVO;
 import lombok.Data;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.factory.Nd4j;
 
 import java.util.LinkedList;
 import java.util.List;
 
-import static com.bachelorthesis.supervised_problem_solving.services.algos.Algorithms.getIndicatorValues;
-import static com.bachelorthesis.supervised_problem_solving.services.algos.Algorithms.getReturns;
 import static com.bachelorthesis.supervised_problem_solving.services.algos.FactorNames.getFactorNames;
 
 @Data
@@ -24,11 +21,6 @@ public class Dl4jLinearRegressionService {
 
     // delta between bars
     private final int[] barDelta = new int[]{5, 10, 15, 20, 25, 30, 60};
-
-    final int rowsToIgnore = FactorNames.getIndicatorDeltas(Indicators.RSI)
-            .get(FactorNames.getIndicatorDeltas(Indicators.RSI).size() - 1);
-
-    final int lengthOfRelevantRows;
 
     /**
      * price - timetable of prices. I MAKE THE ASSUMPTION THAT THIS VARIABLE
@@ -45,34 +37,7 @@ public class Dl4jLinearRegressionService {
 
         final List<String> factorNames = getFactorNames(technicalIndicatorsList, barDelta);
 
-        INDArray factorMatrix = createEmptyFactorMatrix(chartDataVOList, factorNames);
-
-        fillMatrixWithResults(factorNames, technicalIndicatorsList, factorMatrix, chartDataVOList);
+        final INDArray factorMatrix = MService.getFilledMatrix(chartDataVOList, factorNames, barDelta, technicalIndicatorsList);
     }
 
-    private void fillMatrixWithResults(List<String> factorNames, final List<Indicators> technicalIndicatorsList,
-                                       INDArray factorMatrix, final List<ChartDataVO> chartDataVOList) {
-        for (int index = 0; index < factorNames.size(); index++) {
-
-            if (index < barDelta.length) {
-
-                final List<Double> returns = getReturns(chartDataVOList, barDelta[index])
-                        .subList(rowsToIgnore, lengthOfRelevantRows);
-
-                final INDArray indArray = Nd4j.create(returns);
-                factorMatrix.getColumn(index).addi(indArray);
-            } else {
-                final List<List<Double>> indicatorValues = getIndicatorValues(chartDataVOList, technicalIndicatorsList);
-                final INDArray indArray = Nd4j.create(indicatorValues.get(index - barDelta.length).subList(rowsToIgnore, lengthOfRelevantRows));
-                factorMatrix.getColumn(index).addi(indArray);
-            }
-        }
-    }
-
-    private INDArray createEmptyFactorMatrix(List<ChartDataVO> chartDataVOList, List<String> factorNames) {
-        final int columns = factorNames.size();
-        final int rows = lengthOfRelevantRows - barDelta.length;
-
-        return Nd4j.zeros(rows, columns);
-    }
 }
---
+++ src/main/java/com/bachelorthesis/supervised_problem_solving/services/algos/MService.java
@@ -0,0 +1,54 @@
+package com.bachelorthesis.supervised_problem_solving.services.algos;
+
+import com.bachelorthesis.supervised_problem_solving.enums.Indicators;
+import com.bachelorthesis.supervised_problem_solving.services.exchangeAPI.poloniex.vo.ChartDataVO;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.List;
+
+public class MService {
+
+    private static int rows;
+    private static int rowsToIgnore;
+
+    public static INDArray getFilledMatrix(List<ChartDataVO> chartDataVOList, List<String> factorNames,
+                                           final int[] barDeltas, final List<Indicators> technicalIndicatorsList) {
+        MService.rows = chartDataVOList.size();
+        MService.rowsToIgnore = barDeltas.length;
+
+        final INDArray factorMatrix = createEmptyFactorMatrix(factorNames);
+        fillMatrixWithResults(factorNames, technicalIndicatorsList, factorMatrix, chartDataVOList, barDeltas);
+
+        return factorMatrix;
+    }
+
+    private static void fillMatrixWithResults(List<String> factorNames, final List<Indicators> technicalIndicatorsList,
+                                              INDArray factorMatrix, final List<ChartDataVO> chartDataVOList, int[] barDeltas) {
+
+        for (int index = 0; index < factorNames.size(); index++) {
+
+            if (index < barDeltas.length) {
+
+                final List<Double> returns = Algorithms.getReturns(chartDataVOList, barDeltas[index])
+                        .subList(rowsToIgnore, rows);
+
+                final INDArray indArray = Nd4j.create(returns);
+
+                factorMatrix.getColumn(index).addi(indArray);
+            } else {
+                final List<List<Double>> indicatorValues = Algorithms.getIndicatorValues(chartDataVOList, technicalIndicatorsList);
+                final INDArray indArray = Nd4j.create(indicatorValues.get(index - barDeltas.length).subList(rowsToIgnore, rows));
+
+                factorMatrix.getColumn(index).addi(indArray);
+            }
+        }
+    }
+
+    private static INDArray createEmptyFactorMatrix(List<String> factorNames) {
+        final int columns = factorNames.size();
+        final int rows = MService.rows - rowsToIgnore;
+
+        return Nd4j.zeros(rows, columns);
+    }
+}
