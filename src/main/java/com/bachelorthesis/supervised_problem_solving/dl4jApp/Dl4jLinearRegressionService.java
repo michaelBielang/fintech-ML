d37814ddd76e0ad7b0109a57ba454550c53aa875
Michael Bielang <michael.bielang@hs-augsburg.de>
2020-07-13 18:48:38 +0200

    cleanup

--- src/main/java/com/bachelorthesis/supervised_problem_solving/dl4jApp/Dl4jLinearRegressionService.java
+++ src/main/java/com/bachelorthesis/supervised_problem_solving/dl4jApp/Dl4jLinearRegressionService.java
@@ -2,6 +2,7 @@ package com.bachelorthesis.supervised_problem_solving.dl4jApp;
 
 import com.bachelorthesis.supervised_problem_solving.dl4jApp.vo.ResultSet;
 import com.bachelorthesis.supervised_problem_solving.enums.Indicators;
+import com.bachelorthesis.supervised_problem_solving.services.algos.FactorNames;
 import com.bachelorthesis.supervised_problem_solving.services.exchangeAPI.poloniex.enums.Periods;
 import com.bachelorthesis.supervised_problem_solving.services.exchangeAPI.poloniex.vo.ChartDataVO;
 import lombok.Data;
@@ -14,7 +15,6 @@ import java.util.List;
 import static com.bachelorthesis.supervised_problem_solving.services.algos.Algorithms.getIndicatorValues;
 import static com.bachelorthesis.supervised_problem_solving.services.algos.Algorithms.getReturns;
 import static com.bachelorthesis.supervised_problem_solving.services.algos.FactorNames.getFactorNames;
-import static com.bachelorthesis.supervised_problem_solving.services.algos.FactorNames.getIndicatorDeltas;
 
 @Data
 public class Dl4jLinearRegressionService {
@@ -24,6 +24,11 @@ public class Dl4jLinearRegressionService {
 
     // delta between bars
     private final int[] barDelta = new int[]{5, 10, 15, 20, 25, 30, 60};
+
+    final int rowsToIgnore = FactorNames.getIndicatorDeltas(Indicators.RSI)
+            .get(FactorNames.getIndicatorDeltas(Indicators.RSI).size() - 1);
+
+    final int lengthOfRelevantRows;
 
     /**
      * price - timetable of prices. I MAKE THE ASSUMPTION THAT THIS VARIABLE
@@ -43,10 +48,6 @@ public class Dl4jLinearRegressionService {
         INDArray factorMatrix = createEmptyFactorMatrix(chartDataVOList, factorNames);
 
         fillMatrixWithResults(factorNames, technicalIndicatorsList, factorMatrix, chartDataVOList);
-
-        // RSI value is only valid from last day of the time window
-        final int indexOfValidValues = getIndicatorDeltas(Indicators.RSI).get(2);
-
     }
 
     private void fillMatrixWithResults(List<String> factorNames, final List<Indicators> technicalIndicatorsList,
@@ -54,12 +55,15 @@ public class Dl4jLinearRegressionService {
         for (int index = 0; index < factorNames.size(); index++) {
 
             if (index < barDelta.length) {
-                final List<Double> returns = getReturns(chartDataVOList, barDelta[index]);
+
+                final List<Double> returns = getReturns(chartDataVOList, barDelta[index])
+                        .subList(rowsToIgnore, lengthOfRelevantRows);
+
                 final INDArray indArray = Nd4j.create(returns);
                 factorMatrix.getColumn(index).addi(indArray);
             } else {
-                final List<List<Double>> indicatorValues =  getIndicatorValues(chartDataVOList, technicalIndicatorsList);
-                final INDArray indArray = Nd4j.create(indicatorValues.get(index - barDelta.length));
+                final List<List<Double>> indicatorValues = getIndicatorValues(chartDataVOList, technicalIndicatorsList);
+                final INDArray indArray = Nd4j.create(indicatorValues.get(index - barDelta.length).subList(rowsToIgnore, lengthOfRelevantRows));
                 factorMatrix.getColumn(index).addi(indArray);
             }
         }
@@ -67,7 +71,7 @@ public class Dl4jLinearRegressionService {
 
     private INDArray createEmptyFactorMatrix(List<ChartDataVO> chartDataVOList, List<String> factorNames) {
         final int columns = factorNames.size();
-        final int rows = chartDataVOList.size() - barDelta.length;
+        final int rows = lengthOfRelevantRows - barDelta.length;
 
         return Nd4j.zeros(rows, columns);
     }
