5a5f7ce5f53132cdd42fbe5f89024ec48c141a77
Michael Bielang <michael.bielang@hs-augsburg.de>
2020-07-20 10:08:44 +0200

    refactoring

--- src/main/java/com/bachelorthesis/supervised_problem_solving/matlab/Matlab4Regression.java
+++ src/main/java/com/bachelorthesis/supervised_problem_solving/matlab/Matlab4Regression.java
@@ -5,6 +5,9 @@ import com.bachelorthesis.supervised_problem_solving.enums.Indicators;
 import com.bachelorthesis.supervised_problem_solving.services.algos.Algorithms;
 import com.bachelorthesis.supervised_problem_solving.services.algos.ApacheMatrixService;
 import com.bachelorthesis.supervised_problem_solving.services.exchangeAPI.poloniex.vo.ChartDataVO;
+import lombok.val;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
@@ -14,6 +17,8 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import static com.bachelorthesis.supervised_problem_solving.services.algos.FactorNames.getFactorNames;
@@ -21,36 +26,34 @@ import static org.apache.spark.sql.api.r.SQLUtils.createStructField;
 
 public class Matlab4Regression {
 
-
     private final RuntimeDataStorage runtimeDatastorage = new RuntimeDataStorage();
 
-    private final int tradingFrequency = 60;
-
-/*
-    // delta between bars
-    private final int[] barDelta = new int[]{5, 10, 15, 20, 25, 30, 60};
-*/
+    private static final int TRADING_FREQUENCY = 60;
 
     // delta between bars
-    private final int[] barDelta = new int[]{5, 10, 15, 20, 25, 30, 60};
+    private static final int[] BAR_DELTA = new int[]{5, 10, 15, 20, 25, 30, 60};
 
-    public void calculateSignals(final List<ChartDataVO> chartDataVOList, final List<Indicators> technicalIndicatorsList) {
+    public void calculateSignals(final List<ChartDataVO> chartDataVOList, final List<Indicators> technicalIndicatorsList) throws IOException, InterruptedException {
 
-        runtimeDatastorage.findAndSetMaximumMatrixRows(chartDataVOList, technicalIndicatorsList, barDelta, tradingFrequency);
-        validateNumberOfDataPoints(chartDataVOList);
+        init(chartDataVOList, technicalIndicatorsList);
 
-        final List<String> factorNames = getFactorNames(technicalIndicatorsList, barDelta);
-        SparkConf sparkConf = new SparkConf();
-        sparkConf.setMaster("local[2]");
-        sparkConf.setAppName("MLA");
-        SparkSession spark = SparkSession.builder().config(sparkConf).getOrCreate();
-
-        // fill matrix with predictors
+        final List<String> factorNames = getFactorNames(technicalIndicatorsList, BAR_DELTA);
+        final SparkSession spark = setupSpark();
 
         // y in Sample
-        final List<Double> futureReturns = Algorithms.getReturns(chartDataVOList, tradingFrequency);
+        final List<Double> futureReturns = Algorithms.getReturns(chartDataVOList, TRADING_FREQUENCY);
 
-        List<Row> rows = ApacheMatrixService.getRowList(chartDataVOList, factorNames, barDelta, technicalIndicatorsList);
+        final Dataset<Row> indicatorDataSet = getIndicatorDataSet(chartDataVOList, technicalIndicatorsList, factorNames, spark);
+        saveDataToCSV(spark, indicatorDataSet, "indicatorData");
+
+        final Dataset<Double> futureReturnsDataSet = spark.createDataset(futureReturns, Encoders.DOUBLE());
+        saveDataToCSV(spark, futureReturnsDataSet, "futureReturns");
+
+        return;
+    }
+
+    private Dataset<Row> getIndicatorDataSet(List<ChartDataVO> chartDataVOList, List<Indicators> technicalIndicatorsList, List<String> factorNames, SparkSession spark) {
+        List<Row> rows = ApacheMatrixService.getRowList(chartDataVOList, factorNames, BAR_DELTA, technicalIndicatorsList);
         final StructField[] structFields = new StructField[factorNames.size()];
 
         for (int i = 0; i < factorNames.size(); i++) {
@@ -59,22 +62,34 @@ public class Matlab4Regression {
 
         final StructType schemata = DataTypes.createStructType(structFields);
 
-        Dataset<Row> dataSet = spark.createDataFrame(rows, schemata);
-
-        // add column to ds
-
-        Dataset<Double> doubleDataset = spark.createDataset(futureReturns, Encoders.DOUBLE());
-        dataSet = dataSet.join(doubleDataset).withColumnRenamed("value", "futureReturns");
-
-        dataSet.show();
-        dataSet.coalesce(1).
-                write().
-                format("com.databricks.spark.csv").
-                option("header", "false").
-                mode("overwrite").
-                save("bangalalala.csv");
+        return spark.createDataFrame(rows, schemata);
     }
 
+    private SparkSession setupSpark() {
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.setMaster("local[2]");
+        sparkConf.setAppName("MLA");
+        return SparkSession.builder().config(sparkConf).getOrCreate();
+    }
+
+    private void init(List<ChartDataVO> chartDataVOList, List<Indicators> technicalIndicatorsList) {
+        runtimeDatastorage.findAndSetMaximumMatrixRows(chartDataVOList, technicalIndicatorsList, BAR_DELTA, TRADING_FREQUENCY);
+        validateNumberOfDataPoints(chartDataVOList);
+    }
+
+    private void saveDataToCSV(SparkSession spark, Dataset<?> dataSet, String dataType) throws IOException {
+        dataSet.coalesce(1).
+                write().
+                mode("overwrite").
+                format("com.databricks.spark.csv").
+                option("header", "false").
+                save("tmp.csv");
+        val fs = FileSystem.get(spark.sparkContext().hadoopConfiguration());
+        File dir = new File(System.getProperty("user.dir") + "/tmp.csv/");
+        File[] files = dir.listFiles((d, name) -> name.endsWith(".csv"));
+        fs.rename(new Path(files[0].toURI()), new Path(System.getProperty("user.dir") + "/csv/" + dataType + ".csv"));
+        fs.delete(new Path(System.getProperty("user.dir") + "/tmp.csv/"), true);
+    }
 
     private void validateNumberOfDataPoints(List<ChartDataVO> chartDataVOList) {
         if (chartDataVOList.size() < RuntimeDataStorage.getMatrixRowLength()) {
